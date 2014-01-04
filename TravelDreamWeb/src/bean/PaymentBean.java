7fecc564703adc42960c084188e7c587058bcdd9
Francesco Zerbinati <zerbinati.francesco@gmail.com>
2014-01-04 11:14:41 +0100

    fixes and payment bean added ;)

--- TravelDreamWeb/src/bean/PaymentBean.java
+++ TravelDreamWeb/src/bean/PaymentBean.java
@@ -1,10 +1,27 @@
 package bean;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+import coreEJB.AuthenticationEJBLocal;
 
 @ManagedBean(name="Payment")
 @RequestScoped
 public class PaymentBean {
+	
+    @EJB
+    private AuthenticationEJBLocal authEJB;
+	
+    /**
+     * 
+     * @return the buying_list page URL
+     */
+    public String showBuyingList() {
+	if (authEJB.isTDC()) {
+	    return "/user/buying_list?faces-redirect=true";
+	} else
+	    return null;
+    }
 
 }
