903f8d0b9a34f3aa94b3ab836f487a497c03f355
Gianluca Venturini <gianluca.91@gmail.com>
2014-01-11 02:08:25 +0100

    Fixed hash problem in accept_invitation

--- TravelDreamWeb/src/bean/AcceptInvitationBean.java
+++ TravelDreamWeb/src/bean/AcceptInvitationBean.java
@@ -3,14 +3,14 @@ package bean;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import coreEJB.InvitationEJBLocal;
 import exceptions.NotValidInvitationException;
 
 @ManagedBean(name = "AcceptInvitation")
-@RequestScoped
+@ViewScoped
 public class AcceptInvitationBean {
     private String hash;
 
@@ -25,12 +25,14 @@ public class AcceptInvitationBean {
     @EJB
     InvitationEJBLocal invitationEJB;
 
-    public void acceptInvitation() {
+    public String acceptInvitation() {
 	try {
+		System.out.println("Hash: " + this.hash);
 	    invitationEJB.acceptInvitation(hash);
 	} catch (NotValidInvitationException e) {
 	    FacesContext.getCurrentInstance().addMessage("alertInvitation", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Errore", "Al momento e' impossibile confermare l'invito. Riprova."));
 	}
+		return null;
     }
 
 }
